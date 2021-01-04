c16b4a62c634bd7a683d45dd6981708f1b7b92b0
Bratah123 <brandonnguyen3301@gmail.com>
2021-01-03 17:28:56 -0800

    merge sort

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,7 @@
 def quick_sort(my_array):
     # Simple quick sort algo
+    # Time complexity O(n log(n)) Average
+    # Worst O(n^2)
     if len(my_array) <= 1:
         # If array is 1 or less item, it's already sorted so we return
         return my_array
@@ -13,8 +15,35 @@ def quick_sort(my_array):
             less_than_pivot.append(number)
     return quick_sort(less_than_pivot) + [pivot] + quick_sort(greater_than_pivot)
 
+def merge(first_array, second_array):
+    first_arr_size = len(first_array)
+    second_arr_size = len(second_array)
+    merged_array = []
+    first_index, second_index = 0, 0
+
+    while first_arr_size > first_index and second_arr_size > second_index:
+        if first_array[first_index] < second_array[second_index]:
+            merged_array.append(first_array[first_index])
+            first_index += 1
+        else:
+            merged_array.append(second_array[second_index])
+            second_index += 1
+
+    if first_index == first_arr_size:
+        merged_array.extend(second_array[second_index:])
+    else:
+        merged_array.extend(first_array[first_index:])
+    return merged_array
+
 def merge_sort(my_array):
-    pass
+    if len(my_array) <= 1:
+        return my_array
+
+    left = merge_sort(my_array[:int(len(my_array)/2)])
+    right = merge_sort(my_array[int(len(my_array)/2):])
+
+    return merge(left, right)
+
 
 def selection_sort(my_array):
     array_length = len(my_array)
@@ -35,5 +64,8 @@ def fibonacci():
     pass
 
 rand_list = [8, 8, 7, 5, 4, 3, 2, 4, 1]
+merge_list = [1, 3, 5]
+merge_list2 = [2, 4, 6]
 
-print("Selection Sort:", selection_sort(rand_list))
+print("Unsorted:", rand_list)
+print("Sorted:", merge_sort(rand_list))
